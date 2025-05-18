e79830e6df6e153ffacf55891c4c0456868cd4db
emmilish <biblivsemils@gmail.com>
2025-05-18 16:18:35 +0300

    Update statstics.py

--- statstics.py
+++ statstics.py
@@ -1,1 +1,19 @@
+# statistics.py
+import matplotlib.pyplot as plt
+
+def show_horsepower_chart(models):
+    years = [model.year for model in models]
+    horsepower = [model.horsepower for model in models]
+
+    plt.plot(years, horsepower, marker='o')
+    plt.title('Audi RS4 Zirgspēku attīstība')
+    plt.xlabel('Gads')
+    plt.ylabel('Zirgspēki')
+    plt.grid(True)
+    plt.show()
+
+
+def average_price(models):
+    prices = [model.price for model in models]
+    return sum(prices) / len(prices)
 
